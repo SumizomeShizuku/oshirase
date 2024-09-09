562e6b9d6728fb0637f8a67d1d5bef4b0b83e52a
Jo Sawakai <98514826+SumizomeShizuku@users.noreply.github.com>
2024-09-09 09:23:26 +0900

    new

---
+++ hello/src/main/java/com/anestec/hello/ThymeleafController.java
@@ -0,0 +1,27 @@
+package com.anestec.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+
+    @GetMapping("/index")
+    public String showIndex(Model model) {
+        model.addAttribute("name", "小沢");
+        return "index"; // 返回index.html页面
+    }
+
+    @GetMapping("/login")
+    public String showLogin() {
+        return "login";  // 显示 login.html 页面
+    }
+
+
+    // @GetMapping("/index")
+    // public String showIndex() {
+    //     return "index";  // 显示 index.html 页面
+    // }
+    
+}
